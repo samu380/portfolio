3cd8e58332f3c9a7fbcf8092fd74e754396c92d2
Mustafa Saar <mustafa.saar@moguru.de>
2023-03-25 01:50:25 +0100

    added footer and progress label

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faAngular = faAngular;
+}
